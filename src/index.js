424ff53f300fa4fcf486b0fe01ed860ba6d980ec
Scully <51968381+Scullyy@users.noreply.github.com>
2024-04-27 22:56:35 -0700

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,46 +1,36 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const webhook = require('../src/discord.js')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const webhook = require('../src/discord.js');
 
 async function run() {
-  const payload = github.context.payload
-  const repository = payload.repository.name
-  const commits = payload.commits
-  const size = commits.length
+  try {
+    const payload = github.context.payload;
+    const repository = payload.repository.name;
+    const commits = payload.commits;
+    const size = commits.length;
 
-  console.log(`Received payload.`)
-  console.log(`Received ${commits.length}/${size} commits...`)
-  console.log(`------------------------`)
-  console.log(`Full payload: ${JSON.stringify(payload)}`)
-  console.log(`------------------------`)
+    console.log(`Received payload.`);
+    console.log(`Received ${commits.length}/${size} commits...`);
+    console.log(`------------------------`);
+    console.log(`Full payload: ${JSON.stringify(payload)}`);
+    console.log(`------------------------`);
 
-  if (commits.length === 0) {
-    console.log(`No commits, skipping...`)
-    return
+    if (size === 0) {
+      console.log(`No commits, skipping...`);
+      return;
+    }
+    if (payload.sender.type === 'Bot') {
+      console.log(`Commit by bot, skipping...`);
+      return;
+    }
+
+    const id = core.getInput('id');
+    const token = core.getInput('token');
+
+    await webhook.send(id, token, repository, payload.compare, commits, size, payload.pusher.name);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  if (payload.sender.type === 'Bot') {
-    console.log(`Commit by bot, skipping...`)
-    return
-  }
-
-  const id = core.getInput('id')
-  const token = core.getInput('token')
-
-  webhook
-    .send(
-      id,
-      token,
-      repository,
-      payload.compare,
-      commits,
-      size,
-      payload.pusher.name
-    )
-    .catch((err) => core.setFailed(err.message))
 }
 
-try {
-  run()
-} catch (error) {
-  core.setFailed(error.message)
-}
+run();
